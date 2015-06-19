6c3a69e0da074a06b8eef84d949e7cb0af682f89
beegor <beegor@gmail.com>
2015-06-19 16:45:08 +0200

    sending data to web server

--- Baza/Baza.ino
+++ Baza/Baza.ino
@@ -1,19 +1,51 @@
 //Arduino Uno R3 BAZA
 
 #include <RCSwitch.h>
+
+#include <SPI.h>
+#include <Ethernet.h>
+
 RCSwitch receiver = RCSwitch();
 
-void setup() {
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+char server[] = "192.168.0.100";  
   
-  Serial.begin(9600);
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192,168,0,177);
+
+EthernetClient client;
+
+void setup() { 
   receiver.enableReceive(0);  // Receiver on inerrupt 0 => that is pin #2
-  //pinMode(13, OUTPUT);
-  //digitalWrite(13, LOW);
-  //Bridge.begin();
+ 
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  
+  Serial.println("connecting...");
+  if (client.connect(server, 9999)) {
+    Serial.println("connected");
+  } 
+  else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+
 }
 
 void loop() {
-  //Bridge.begin();
   if (receiver.available()) {
     dataReceived();
   }
@@ -21,16 +53,38 @@ void loop() {
 }
 
 void dataReceived(){
- long value = receiver.getReceivedValue();
-    Serial.println("R3 received info , motion detected "); 
-    Serial.print(value);
-    Serial.println(" seconds ago");
-    receiver.resetAvailable();
-     //sendToServer(value);
-  }
+  long value = receiver.getReceivedValue();
 
-//void sendToServer(long motionSecondsAgo)
-  //HttpClient client;
-   //client.get("http://192.168.0.100:8080/portal");
+  int motionBeforSecond = value % 10000;
+  int temp = (value / 100000) % 100 - 50; // we added 50 on nano side
+  int lightOnValue = value / 10000000;
+  String lightOn = lightOnValue == 1 ? "On" : "Off";
+  
+  Serial.println("R3 received info\t"); 
+  Serial.print("Motion before ");
+  Serial.print(motionBeforSecond);
+  Serial.print(" seconds\tTemp: ");
+  Serial.print(temp);
+  Serial.print("\tLigh: ");
+  Serial.println(lightOn);
+  
+  receiver.resetAvailable();
+  sendToServer(motionBeforSecond, temp, lightOn);
+}
 
+void sendToServer(int motionBeforSecond, int temp, String lightOn ) {
+
+    client.print("GET /receive?");
+    client.print("motionBeforeSecond=");
+    client.print(motionBeforSecond);
+    client.print("&temp=");
+    client.print(temp);
+    client.print("&lightOn=");
+    client.print(lightOn);
+    client.println(" HTTP/1.1");
+    client.println("Host: 192.168.0.100");
+    client.println("Connection: keep-alive");
+    client.println();
+    client.flush();
+}
 
--- Ured/Ured.ino
+++ Ured/Ured.ino
@@ -1,43 +1,92 @@
+#include <dht.h>
+
 //Elektronika u uredu
 
 #include <RCSwitch.h>
 #include <Time.h>
+
 RCSwitch transmiter = RCSwitch();
 
 int ured = 1;
 int lastmotiontime = -1;
+dht DHT;
 
 void setup() {
   Serial.begin(9600);
-  transmiter.enableTransmit(10); //s motion senzora podatci
-  pinMode(2, INPUT_PULLUP);      //transmitter na pin2
-  attachInterrupt(0, motionDetected, CHANGE);
+  transmiter.enableTransmit(10); //transmitter na pin 10
+  pinMode(2, INPUT_PULLUP);   //s motion senzora podatci, pin 2
+  attachInterrupt(0, sendAlert, CHANGE);
   randomSeed(A0);
+  
 }
-
 
 void loop() {
-  int currentSecond = now();
-  if(lastmotiontime == -1){
-    Serial.println("No motion detected");
-  }
-  else {
-     int lastMotionBefore = currentSecond - lastmotiontime;
-     Serial.print("Motion detected: ");
-     Serial.print(lastMotionBefore);
-     Serial.println(" seconds ago");   
-     int lightOn = 1;
-     int temp = 23;
-     long data = lightOn * 10000000;
-     data += temp * 100000;
-     data += lastMotionBefore;  
-     transmiter.send(data,32);
-   }
-   delay(5000);
+
+  // ocitanje senzora
+  int lightOn = 2;
+  
+  int lastMotionBefore = getLastMotionBefore();
+  int tempAndHum[] = {0,0};
+  getTempAndHumidity(tempAndHum);
+  
+  int temp = tempAndHum[0] + 50; //because we dont want negative temperatures
+   
+  
+  //slanje podataka ukomponiranih u 1 long broj
+  long data = lightOn * 10000000;
+  data =  data + (temp * 100000);
+  data = data + lastMotionBefore;
+  Serial.print("Sending to baza: ");Serial.println(data);
+  transmiter.send(data, 32);
+
+  // cakanje 5 sekundi
+  delay(5000);
 }
 
 
-void motionDetected(){
+int getLastMotionBefore(){
+  
+  int currentSecond = now();
+  if (lastmotiontime == -1) {
+    Serial.println("No motion detected");
+    return 10000;
+  }
+  else {
+    int lastMotionBefore = currentSecond - lastmotiontime;
+    Serial.print("Motion detected ");
+    Serial.print(lastMotionBefore);
+    Serial.println(" seconds ago");
+    return lastMotionBefore;
+  }
+}
+
+
+void getTempAndHumidity(int tempAndHum[]){
+  
+  int termRead = DHT.read11(6);
+  switch (termRead)
+  {
+    case DHTLIB_OK:  
+      Serial.print("OK,\t"); 
+      break;
+    default: 
+      Serial.print("Unknown error,\t"); 
+      break;
+  }
+
+  tempAndHum[0] = DHT.temperature;
+  tempAndHum[1] = DHT.humidity;
+
+  Serial.print("Temperature: ");
+  Serial.print(DHT.temperature, 1);
+  Serial.print(" ,\tHumidity: ");
+  Serial.println(DHT.humidity ,1);
+  
+}
+
+
+
+void sendAlert() {
   Serial.println("Motion detected from sensor ");
   lastmotiontime = now();
 }
