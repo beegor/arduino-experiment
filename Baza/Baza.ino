b1fa095f2aa68fda80cb011d765b569eaf88b8cf
beegor <beegor@gmail.com>
2015-06-19 12:15:10 +0200

    Initial commit

---
+++ Baza/Baza.ino
@@ -0,0 +1,36 @@
+//Arduino Uno R3 BAZA
+
+#include <RCSwitch.h>
+RCSwitch receiver = RCSwitch();
+
+void setup() {
+  
+  Serial.begin(9600);
+  receiver.enableReceive(0);  // Receiver on inerrupt 0 => that is pin #2
+  //pinMode(13, OUTPUT);
+  //digitalWrite(13, LOW);
+  //Bridge.begin();
+}
+
+void loop() {
+  //Bridge.begin();
+  if (receiver.available()) {
+    dataReceived();
+  }
+  delay(1000);
+}
+
+void dataReceived(){
+ long value = receiver.getReceivedValue();
+    Serial.println("R3 received info , motion detected "); 
+    Serial.print(value);
+    Serial.println(" seconds ago");
+    receiver.resetAvailable();
+     //sendToServer(value);
+  }
+
+//void sendToServer(long motionSecondsAgo)
+  //HttpClient client;
+   //client.get("http://192.168.0.100:8080/portal");
+
+
---
+++ Ured/Ured.ino
@@ -0,0 +1,44 @@
+//Elektronika u uredu
+
+#include <RCSwitch.h>
+#include <Time.h>
+RCSwitch transmiter = RCSwitch();
+
+int ured = 1;
+int lastmotiontime = -1;
+
+void setup() {
+  Serial.begin(9600);
+  transmiter.enableTransmit(10); //s motion senzora podatci
+  pinMode(2, INPUT_PULLUP);      //transmitter na pin2
+  attachInterrupt(0, motionDetected, CHANGE);
+  randomSeed(A0);
+}
+
+
+void loop() {
+  int currentSecond = now();
+  if(lastmotiontime == -1){
+    Serial.println("No motion detected");
+  }
+  else {
+     int lastMotionBefore = currentSecond - lastmotiontime;
+     Serial.print("Motion detected: ");
+     Serial.print(lastMotionBefore);
+     Serial.println(" seconds ago");   
+     int lightOn = 1;
+     int temp = 23;
+     long data = lightOn * 10000000;
+     data += temp * 100000;
+     data += lastMotionBefore;  
+     transmiter.send(data,32);
+   }
+   delay(5000);
+}
+
+
+void motionDetected(){
+  Serial.println("Motion detected from sensor ");
+  lastmotiontime = now();
+}
+
